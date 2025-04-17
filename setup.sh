1ec5d682cb4c68fc49716b785ae1bf5be721d469
user285k <deku55k@gmail.com>
2025-04-17 15:44:47 +0530

    added more

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 echo "Proceeding with post installation"
-echo "Installing Packages."
+echo "Installing Packages..."
 
-sudo pacman -Sy --noconfirm base-devel git libx11 libxinerema libxft xf86-input-synaptics xf86-video-intel xorg-server xorg-xinit xorg-xrandr xwallpaper bluez cmake mpv zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse picom brightnessctl usbutils intel-ucode intel-media-driver mesa dosfstools networkmanager ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono noto-fonts-emoji 
+sudo pacman -Sy --noconfirm base-devel git libx11 libxinerema libxft xf86-input-synaptics xf86-video-intel xorg-server xorg-xinit xorg-xrandr xwallpaper bluez cmake mpv zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse picom python python-pip python-pywal python-setuptools fzf brightnessctl usbutils intel-ucode intel-media-driver mesa dosfstools networkmanager ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono noto-fonts-emoji
+
+echo "Setting up dotfiles"
+sleep 2
+git clone --bare https://github.com/shivjeet1/dotfiles.git $HOME/.dotfiles/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+
+echo "configuring . . ."
+sleep 1
+export DISPLAY=3
+
+
