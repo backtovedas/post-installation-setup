b2e2cdf184552cf185c0753ddc0bf3b86d1afd8e
user285k <deku55k@gmail.com>
2025-04-18 02:18:00 +0530

    works

--- setup.sh
+++ setup.sh
@@ -24,18 +24,19 @@ export DISPLAY=3
 
 source $HOME/.zprofile 2> /dev/null
 wal -s -i $HOME/post-installation-setup/art.jpg
-sed '/urg/d' -i $XDG_CACHE_HOME/wal/colors-wal-dwm.h
-sed '31s/0/256/' -i $XDG_CACHE_HOME/wal/colors-wal-st.h
-sed '24s/"[^"]*]"/"#000000"/' -i $XDG_CACHE_HOME/wal/colors-wal-dmenu.h
-sed "s/.*foreground.*$(grep foreground $XDG_CACHE_HOME/wal/colors.Xresources | head -n 1 | sed s/\*/Sxiv\./g)/" -i $XRESOURCES
-sed "s/.*background.*/$(grep background $XDG_CACHE_HOME/wal/colors.Xresources | head -n 1 | sed s/\*/Sxiv\./g)/" -i $XRESOURCES
-
 echo "Setting up DWM"
 mkdir -p $HOME/.local/src/
 git clone --bare https://github.com/shivjeet1/dwm.git $HOME/.local/src/dwm/
 git clone --bare https://github.com/shivjeet1/dmenu.git $HOME/.local/src/dmenu/
 git clone --bare https://github.com/shivjeet1/slstatus.git $HOME/.local/src/slstatus/
 git clone --bare https://github.com/shivjeet1/st.git $HOME/.local/src/st/
+
+echo "removing irrelevant content"
+sed '/urg/d' -i $XDG_CACHE_HOME/wal/colors-wal-dwm.h
+sed '31s/0/256/' -i $XDG_CACHE_HOME/wal/colors-wal-st.h
+sed '24s/"[^"]*]"/"#000000"/' -i $XDG_CACHE_HOME/wal/colors-wal-dmenu.h
+sed "s/.*foreground.*$(grep foreground $XDG_CACHE_HOME/wal/colors.Xresources | head -n 1 | sed s/\*/Sxiv\./g)/" -i $XRESOURCES
+sed "s/.*background.*/$(grep background $XDG_CACHE_HOME/wal/colors.Xresources | head -n 1 | sed s/\*/Sxiv\./g)/" -i $XRESOURCES
 
 user_correction(){
 	sed -i "s/shiv/$USER/" $HOME/.local/src/dwm/config.h
@@ -47,7 +48,7 @@ user_correction(){
 }
 
 case $USER in
-	shiv)
+	user)
 		cd $HOME/.local/src/dwm; sudo make clean install
 		cd $HOME/.local/src/st; sudo make clean install
 		cd $HOME/.local/src/dmenu; sudo make clean install
