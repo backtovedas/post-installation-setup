7a2a7e46aa7cd7523010d014273dc21a18083981
user285k <deku55k@gmail.com>
2025-04-17 22:24:11 +0530

    package name corrected

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "Proceeding with post installation"
 echo "Installing Packages..."
 
-sudo pacman -Sy --noconfirm base-devel git libx11 libxinerema libxft xf86-input-synaptics xf86-video-intel xorg-server xorg-xinit \
+sudo pacman -Sy --noconfirm base-devel git libx11 libxinerama libxft xf86-input-synaptics xf86-video-intel xorg-server xorg-xinit \
     xorg-xrandr xwallpaper bluez cmake mpv zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse picom \
     python python-pip python-pywal python-setuptools fzf brightnessctl usbutils intel-ucode intel-media-driver mesa dosfstools \
     networkmanager ttf-jetbrains-mono ttf-nerd-fonts-symbols-mono noto-fonts-emoji
